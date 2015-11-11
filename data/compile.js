e5ea0ce24d4eb56932cc0fcb28afbeccb29a835d
Jauke <joke@biloucorp.com>
2015-11-11 22:40:23 +0100

    append some functions

--- data/compile.js
+++ data/compile.js
@@ -1,9 +1,59 @@
 var fs = require('fs');
 
-var scales = JSON.parse(fs.readFileSync('scales.json', 'utf8'));
-var codage = JSON.parse(fs.readFileSync('codage.json', 'utf8'));
-var combin = JSON.parse(fs.readFileSync('combin.json', 'utf8'));
-var data = {};
+/* init src data */
+
+var scales = JSON.parse(fs.readFileSync('src/scales.json', 'utf8'));
+var codage = JSON.parse(fs.readFileSync('src/codage.json', 'utf8'));
+var combin = JSON.parse(fs.readFileSync('src/combin.json', 'utf8'));
+
+/* init data */
+
+var data = {
+  scales    : scales,
+  intervals : codage.intervals,
+  keyboard  : codage.keyboard
+};
+
+/* scale interval first codification */
+
+function codeIntervals(scaleArr) {
+  return scaleArr.map(function(value) {
+    return codage.intervals[value].code;
+  });
+};
+
+/* scale combination */
+
+function allCombinations(category, scaleCodeArr) {
+  return combin[category].variations.map(function(row) {
+    return row.map(function(value, index) {
+      return (scaleCodeArr[index] || 12) + value * 12;
+    });
+  });
+};
+
+/* get intervals between values */
+
+function getIntervals(arr) {
+  var sorted = arr.sort(function(a, b) {a - b});
+  var result = [];
+  for (var i = 1; i < sorted.length; i++) {
+    result.push(sorted[i] - sorted[i-1]);
+  };
+  return result;
+};
+
+/* exclude combinations */
+
+function isToExclude(category, combination) {
+  return getIntervals(combination).some(function(el) {
+    return combin[category].exclusions.some(function(ex) {
+      return el == ex;
+    });
+  });
+};
+
+/* write data file */
 
 function writeData() {
   var content = "var data = " + JSON.stringify(data, null, '  ');
@@ -15,4 +65,8 @@ function writeData() {
   })
 };
 
+console.log(getIntervals([0,5,10,15,20,25]));
+console.log(isToExclude("5 notes", [2,8,10,11]));
+console.log(isToExclude("5 notes", [1,7,10,12]));
+console.log(isToExclude("5 notes", [1,7,5,12]));
 writeData();
--- data/data.js
+++ data/data.js
@@ -1,1 +1,212 @@
-var data = {}
+var data = {
+  "scales": {
+    "5 notes": {
+      "Balinaise": [
+        "T",
+        "2m",
+        "3m",
+        "5",
+        "6m"
+      ],
+      "Chinoise (deuxième forme)": [
+        "T",
+        "3",
+        "4+",
+        "5",
+        "7"
+      ],
+      "Chinoise (première forme)": [
+        "T",
+        "2",
+        "4",
+        "5",
+        "6"
+      ],
+      "Hirajoshi": [
+        "T",
+        "2",
+        "2m",
+        "5",
+        "6m"
+      ]
+    },
+    "6 notes": {
+      "Augmentée": [
+        "T",
+        "3m",
+        "3",
+        "5",
+        "6m",
+        "7"
+      ],
+      "Bahar": [
+        "T",
+        "2",
+        "3m",
+        "4",
+        "5",
+        "7m"
+      ],
+      "Bitonal majeur chromatique": [
+        "T",
+        "3m",
+        "3",
+        "5°",
+        "5",
+        "7"
+      ],
+      "Bitonal mineur chromatique": [
+        "T",
+        "2",
+        "3m",
+        "5°",
+        "5",
+        "7"
+      ],
+      "Blues diminué (deuxième forme)": [
+        "T",
+        "3m",
+        "3",
+        "5°",
+        "5",
+        "6"
+      ],
+      "Enigmatique": [
+        "T",
+        "2m",
+        "3",
+        "4+",
+        "6m",
+        "7m"
+      ]
+    }
+  },
+  "intervals": {
+    "2": {
+      "code": 2,
+      "name": "Seconde majeure"
+    },
+    "3": {
+      "code": 4,
+      "name": "Tierce majeure"
+    },
+    "4": {
+      "code": 5,
+      "name": "Quarte juste"
+    },
+    "5": {
+      "code": 7,
+      "name": "Quinte juste"
+    },
+    "6": {
+      "code": 9,
+      "name": "Sixte majeure"
+    },
+    "7": {
+      "code": 11,
+      "name": "Septième majeure"
+    },
+    "T": {
+      "code": 0,
+      "name": "Prime"
+    },
+    "2m": {
+      "code": 1,
+      "name": "Seconde mineure"
+    },
+    "2+": {
+      "code": 3,
+      "name": "Seconde augmentée"
+    },
+    "3m": {
+      "code": 3,
+      "name": "Tierce mineure"
+    },
+    "4°": {
+      "code": 4,
+      "name": "Quarte diminuée"
+    },
+    "4+": {
+      "code": 6,
+      "name": "Quarte augmentée"
+    },
+    "5°": {
+      "code": 6,
+      "name": "Quinte diminuée"
+    },
+    "5+": {
+      "code": 8,
+      "name": "Quinte augmentée"
+    },
+    "6m": {
+      "code": 8,
+      "name": "Sixte mineure"
+    },
+    "6+": {
+      "code": 9,
+      "name": "Sixte augmentée"
+    },
+    "7°": {
+      "code": 9,
+      "name": "Septième diminuée"
+    },
+    "7m": {
+      "code": 10,
+      "name": "Septième mineure"
+    }
+  },
+  "keyboard": {
+    "full": [
+      "Cb",
+      "C",
+      "C#",
+      "Ebb",
+      "Eb",
+      "E",
+      "E#",
+      "Gb",
+      "G",
+      "G#",
+      "Bbb",
+      "Bb",
+      "B",
+      "B#",
+      "Db",
+      "D",
+      "D#",
+      "Fb",
+      "F",
+      "F#",
+      "F##",
+      "Ab",
+      "A",
+      "A#"
+    ],
+    "usual": [
+      "B",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#",
+      "A",
+      "A#",
+      "B",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "F",
+      "F#",
+      "G",
+      "G#",
+      "A",
+      "A#"
+    ]
+  }
+}
