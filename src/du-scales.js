2564a7d2702f420326a660d0cc0c4cd03acd4d1d
Jauke <joke@biloucorp.com>
2015-11-17 01:57:21 +0100

    fix firefox

--- src/du-scales.js
+++ src/du-scales.js
@@ -1,6 +1,6 @@
-var content = d3.select(".content");
-
-var svg  =  content.select("svg"),
+var navigation = d3.select("#navigation"),
+    content = d3.select(".content"),
+    svg  =  content.select("svg"),
     title = content.select("h2");
     
 var notes = "usual",
@@ -16,28 +16,43 @@ var touch = svg
     .data(data.keyboard.coordinates)
   .enter().append("g")
     .attr("class", "touch")
-    .on("click", function(d,i) {transpose(i);});
+    .on("click", transpose)
+    .on("mouseover", mouseover)
+    .on("mouseout", mouseout);
 
 var hexa = touch
   .append("polygon")
     .attr("class", "hexagon")
     .attr("points", "0,11.547 10,5.774 10,-5.774 0,-11.547 -10,-5.774 -10.000,5.774")
     .attr("transform", function(d,i) {return "translate("+ d.x +","+ d.y +")";})
-    .style("fill", function(d,i){return lighten(i)});
+    .style("fill", lighten);
 
 var text = touch
   .append("text")
     .attr("class", "notation")
     .attr("x", function(d) {return d.x})
     .attr("y", function(d,i) {return d.y + 3})
-    .text(function(d,i) {return keys(i)});
-    
+    .text(keys);
+
+function mouseover(d) {
+  var dx = -d.x*(0.9-1),
+      dy = -d.y*(0.9-1);
+  d3.select(this).transition().duration(100)
+    .attr("transform","translate("+dx+","+dy+")scale(0.9)");
+};
+
+function mouseout(d) {
+  d3.select(this)
+    .transition().duration(100)
+    .attr("transform","translate(0,0)scale(1)")
+};
+
 function range(i) {
   i = (i - transpo)%24;
   return (i >= 0) ? i : i + 24;
 };
     
-function lighten(i) {
+function lighten(d,i) {
   var sc = data.scales[category][currScale].variations[variation];
   return (range(i) == 0)
     ? "#FFC057"
@@ -46,11 +61,11 @@ function lighten(i) {
       : "#F3F3F3";
 };
 
-function keys(i) {
+function keys(d,i) {
   return data.keyboard[notes][i%24];
 };
 
-function transpose(i) {
+function transpose(d,i) {
   transpo = i%24;
   update();
 };
@@ -60,8 +75,8 @@ function newTitle() {
 };
 
 function update(){
-  text.text(function(d,i){return keys(i)});
-  hexa.style("fill", function(d,i){return lighten(i)});
+  text.text(keys);
+  hexa.style("fill", lighten);
   newTitle();
 };
 
