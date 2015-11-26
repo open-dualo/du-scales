a49a04409c7537efaaa10081c1bb57b865bd4382
Jauke <joke@biloucorp.com>
2015-11-26 02:35:25 +0100

    append depreciated pattern

--- beta/src/data/data_compiler.js
+++ beta/src/data/data_compiler.js
@@ -9,8 +9,10 @@ var multi_data = [
   {data: 'scales-wikipedia.tsv',    source: {name: "Wikipedia", url: "https://en.wikipedia.org/wiki/List_of_musical_scales_and_modes"}},
   {data: 'scales-harmonics.tsv',    source: {name: "Harmonics.com", url: "http://www.harmonics.com/scales/"}}
 ];
+
 var codage = JSON.parse(fs.readFileSync('codage.json', 'utf8'));
 var packed_data = [];
+
 var data = {
   scales : {},
   intervals : codage.intervals,
@@ -32,9 +34,81 @@ function onlyUnique(value, index, self) {
     return self.indexOf(value) === index;
 }
 
+/* get intervals between values */
+
+function getIntervals(arr) {
+  var sorted = arr.slice(0).sort(function(a, b) {return a - b});
+  sorted.push(24);
+  var result = [];
+  for (var i = 1; i < sorted.length; i++) {
+    result.push(sorted[i] - sorted[i-1]);
+  };
+  return result;
+};
+
+/* get balance factor between right and left keyboard */
+
+function balance(code) {
+  var length = code.length;
+  var odd = 0;
+  for (var value of code) {
+    if (value > 11) {
+      odd += 1;
+    };
+  };
+  return Math.abs(0.5 - (length - odd - 1)/(length-1));
+};
+
+/* count depreciated patterns in intervals */
+
+function depreciation(code) {
+  var dep = 0;
+  var inter = getIntervals(code);
+  inter = inter.concat(inter[0]);
+  for (var i = 0; i < inter.length-1; i++) {
+    if (inter[i] === 1 ||
+        inter[i] === 2 && (inter[i-1] === 5 || inter[i+1] === 5))
+    {
+      dep++;
+    };
+  };
+  return dep;
+};
+
+/* add combinations to data */
+
+function getCombinations(code) {
+  var magic = [
+    [0,13,14,3,4,17,18,7,20,21,10,11,24],
+    [0,13,14,3,4,17,6,7,20,21,10,11,24]
+  ];
+  var nb_result = code.some(function(i){return i === 6}) ? 2 : 1;
+  var result = (nb_result === 2) ? [[],[]] : [[]];
+  for (var interval of code) {
+    result[0].push(magic[0][interval]);
+    if (nb_result === 2) {
+      result[1].push(magic[1][interval]);
+    };
+  };
+  if (nb_result === 2) {
+    var b = [balance(result[0]),
+             balance(result[1]),
+             depreciation(result[0]),
+             depreciation(result[1])];
+    if (b[0] > b[1] || (b[0] === b[1] && b[2] > b[3])) {
+      result = [result[1], result[0]];
+    };
+  };
+  return result;
+};
+
 function registerScale(element, index, source){
-  var code = element.code.split(" ").map(function(value){return codage.source[source.name][value]});
-  var i = packed_data.findIndex(function(e){return e.code.toString() === code.toString()});
+  var code = element.code.split(" ").map(function(value){
+      return codage.source[source.name][value];
+    });
+  var i = packed_data.findIndex(function(e){
+      return e.code.toString() === code.toString();
+    });
   if (i === -1) {
     packed_data.push({
       category: code.length + " notes",
@@ -69,62 +143,6 @@ function writeData() {
     if(err) {return console.log(err)};
     console.log("data.tsv successfully compiled.");
   });
-};
-
-/* get intervals between values */
-
-function getIntervals(arr) {
-  var sorted = arr.slice(0).sort(function(a, b) {return a - b});
-  sorted.push(24);
-  var result = [];
-  for (var i = 1; i < sorted.length; i++) {
-    result.push(sorted[i] - sorted[i-1]);
-  };
-  return result;
-};
-
-/* get balance factor between right and left keyboard */
-
-function balance(code) {
-  var length = code.length;
-  var odd = 0;
-  for (var value of code) {
-    if (value > 11) {
-      odd += 1;
-    };
-  };
-  return Math.abs(0.5 - (length - odd - 1)/(length-1));
-};
-
-/* match depreciated intervals */
-
-function balance2(code) {
-  return getIntervals(code).some(function(value){return value===1});
-};
-
-/* add combinations to data */
-
-function getCombinations(code) {
-  var magic = [
-    [0,13,14,3,4,17,6,7,20,21,10,11,24],
-    [0,13,14,3,4,17,18,7,20,21,10,11,24]
-  ];
-  var nb_result = code.some(function(i){return i === 6}) ? 2 : 1;
-  var result = (nb_result === 2) ? [[],[]] : [[]];
-  for (var interval of code) {
-    result[0].push(magic[0][interval]);
-    if (nb_result === 2) {
-      result[1].push(magic[1][interval]);
-    };
-  };
-  if (nb_result === 2) {
-    var b = [balance(result[0]),  balance(result[1]),
-              balance2(result[0]), balance2(result[1])];
-    if (b[0] > b[1] || (b[0] === b[1] && b[2] && !b[3])) {
-      result = [result[1], result[0]];
-    };
-  };
-  return result;
 };
 
 packData();
